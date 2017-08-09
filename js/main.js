91018eadd2e9663ede88fef5621454eae3dbed02
Edson Júnior <ebf.junin@gmail.com>
2017-08-09 19:51:34 -0300

    Mudanças no baseurl

--- js/main.js
+++ js/main.js
@@ -6,32 +6,25 @@ sitemap:
 
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
-    console.log("1");
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     currentWidth = $('.panel-cover').width()
-  console.log("2");
     if (currentWidth < 960) {
-      console.log("3");
       $('.panel-cover').addClass('panel-cover--collapsed');
       $('.content-wrapper').addClass('animated slideInRight');
       $('.navigation--social').css('margin-left', '0');
     } else {
-      console.log("4");
       $('.panel-cover').css('max-width', currentWidth)
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
   })
 
   if (window.location.hash && window.location.hash == '#blog') {
-    console.log("5");
     $('.panel-cover').addClass('panel-cover--collapsed');
     $('.navigation--social').css('margin-left', '0');
   }
 
-console.log(window.location.pathname);
-console.log("{{ site.baseurl }}");
-  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
-    console.log("6");
+  // if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
+  if (window.location.pathname !== '/') {
     $('.panel-cover').addClass('panel-cover--collapsed');
     $('.navigation--social').css('margin-left', '0');
   }
